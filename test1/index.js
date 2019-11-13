19a6cbeb931eed6db95004f5f400b22cc2d64a00
Dale <32755677+Garanox@users.noreply.github.com>
2019-11-13 15:08:14 -0500

    updated more

--- test1/index.js
+++ test1/index.js
@@ -31,22 +31,9 @@ var json = {
                     
                 }
             ]
-        }, {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "magnacarta",
-                    title: "What is the Magna Carta?",
-                    choicesOrder: "random",
-                    choices: [
-                        "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
-                    ],
-                    correctAnswer: "The foundation of the British parliamentary system"
-                }
-            ]
-        }
+        }, 
     ],
-    completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
+    
 };
 
 window.survey = new Survey.Model(json);
